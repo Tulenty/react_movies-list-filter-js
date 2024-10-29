13d3fda341d56fab84dc8f513bee332c603ad280
missk <rostock.vasiljev@gmail.com>
2024-10-29 12:32:36 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
-import { useState } from 'react';
 
 export const App = () => {
   const [query, setQuery] = useState('');
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     setQuery(event.target.value);
-  }
+  };
 
-  const visibleMovies = moviesFromServer.filter((movie) => (
-    movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
-    movie.description.toLowerCase().includes(query.trim().toLowerCase())
-  ));
+  const visibleMovies = moviesFromServer.filter(
+    movie =>
+      movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
+      movie.description.toLowerCase().includes(query.trim().toLowerCase()),
+  );
 
-  return(
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-              value = {query}
-              onChange={handleInputChange}
-            />
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={handleInputChange}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={visibleMovies} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-)};
+  );
+};
